eb86e3e4d75e7abc33ebe5ac438bb8da931ed99f
rakibulislam93 <rakibulislamarif793@gmail.com>
2025-10-03 18:50:51 +0600

    connect new database

--- core/settings.py
+++ core/settings.py
@@ -1,9 +1,7 @@
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-load_dotenv()
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -12,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY")
 
 
 
@@ -118,14 +116,12 @@ ASGI_APPLICATION = 'core.asgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT"),
-        "OPTIONS": {
-            "sslmode": "require"
-        }
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD':config("DB_PASSWORD"),
+        'HOST':config("DB_HOST"),
+        'PORT': config("DB_PORT"),
+        
     }
 }
 # Password validation
