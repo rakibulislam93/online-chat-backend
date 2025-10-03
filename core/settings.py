e322be0a992fe7aa589509f981f8777e71c073ef
rakibulislam93 <rakibulislamarif793@gmail.com>
2025-10-03 17:25:25 +0600

    some changes

--- core/settings.py
+++ core/settings.py
@@ -123,6 +123,9 @@ DATABASES = {
         'PASSWORD': os.environ.get("DB_PASSWORD"),
         'HOST': os.environ.get("DB_HOST"),
         'PORT': os.environ.get("DB_PORT"),
+        "OPTIONS": {
+            "sslmode": "require"
+        }
     }
 }
 # Password validation
